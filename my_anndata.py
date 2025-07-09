ee70e54ff05d75a2d04e13785de64076471d6841
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-09 16:49:02 -0500

    Update my_anndata.py
    Conversion into functions. Added documentation

--- my_anndata.py
+++ my_anndata.py
@@ -11,53 +11,82 @@ import matplotlib as mp
 from tkinter import filedialog, simpledialog
 from scipy.sparse import csr_matrix
 
+def load():
+    '''
+    Function to load the file and checks for correct file type (h5ad)
 
-# Loads the file
-file_path = filedialog.askopenfilename(
-    title="Select .h5ad file",
-    filetypes=[("h5ad files", "*.h5ad")]
-)
+    Returns
+    ----------
+    adata: anndata object
+        prints adata contents
+    '''
+    # Load the file
+    file_path = filedialog.askopenfilename(
+        title="Select .h5ad file",
+        filetypes=[("h5ad files", "*.h5ad")])
 
-if file_path:
-    adata = ad.read_h5ad(file_path)
-    print(f"Loaded {adata.shape[0]} cells and {adata.shape[1]} genes.")
-else:
-    print("No file selected.")
-
-print(adata)
-
-
-# Convert from pandas array to Numpy array ('X_umap" name and Numpy array required by Scanpy)
-adata.obsm['X_umap'] = adata.obsm['UMAP'].values
-
-
-# Prompts users for annotations to plot
-colors = []
-while True:
-    options = "\n".join(adata.obs.columns)
-
-    selected_color = simpledialog.askstring(
-        "Choose annotation", 
-        f"Available annotations:\n{options}\n\nWhich one would you like to color by?")
-
-    if not selected_color:
-        break
-
-    if selected_color not in adata.obs.columns:
-        print(f"'{selected_color}' is not a valid annotation.")
+    # Check for correct file type
+    if file_path:
+        adata = ad.read_h5ad(file_path)
+        print(f"Loaded {adata.shape[0]} cells and {adata.shape[1]} genes.")
     else:
-        colors.append(selected_color)
+        print("No file selected.")
 
-    another = simpledialog.askstring(
-        "Add another?",
-        "Would you like to add another annotation? (yes/no)")
+    # Shows user what is in anndata
+    print(adata)
+    return adata
+
+
+def plot(adata):
+    '''
+    Function that takes anndata object and plots the annotations that user specifies
+
+    Parameters
+    ----------
+    adata: anndata object
+
+    Raises
+    ----------
+    TypeError
+        If adata is not anndata object
+
+    Returns
+    ----------
+    none
+        Displays graphs for annotations chosen
+    '''
+    # Check for correct file type
+    if not isinstance(adata, ad.AnnData):
+        raise TypeError("Input must be an AnnData object.")
+
+    # Convert from pandas array to Numpy array ('X_umap" name and Numpy array required by Scanpy)
+    adata.obsm['X_umap'] = adata.obsm['UMAP'].values
+
+    # Prompts users for annotations to plot
+    colors = []
+    while True:
+        options = "\n".join(adata.obs.columns)
+
+        selected_color = simpledialog.askstring(
+            "Choose annotation", 
+            f"Available annotations:\n{options}\n\nWhich one would you like to color by?")
+
+        if not selected_color:
+            break
+
+        if selected_color not in adata.obs.columns:
+            print(f"'{selected_color}' is not a valid annotation.")
+        else:
+            colors.append(selected_color)
+
+        another = simpledialog.askstring(
+            "Add another?",
+            "Would you like to add another annotation? (yes/no)")
     
-    if another is None or another.lower() != 'yes':
-        break
+        if another is None or another.lower() != 'yes':
+            break
 
-    
-
-# Plots annotations
-sc.pl.umap(adata, color=colors)
+    # Plots annotations
+    sc.pl.umap(adata, color=colors)
 
 
